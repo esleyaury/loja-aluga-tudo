d79d30202d5f824907161a63356e6b00ade7eba0
Mateus Freitas <mateus.fgomes@upe.br>
2026-06-18 11:37:58 -0300

    create: mas praticas, realizadas?

---
+++ src/main/java/com/upe/loja/repository/entity/Cliente.java
@@ -0,0 +1,5 @@
+package com.upe.loja.entity;
+
+public class Cliente extends Usuario{
+	//Criar atributos de cliente e seus métodos.
+}
---
+++ src/main/java/com/upe/loja/repository/entity/Usuario.java
@@ -0,0 +1,28 @@
+package com.upe.loja.entity;
+
+public abstract class Usuario{
+	
+	private String cpf;
+	private String nome;
+	private String email;
+	private String endereco;
+	
+	public void Usuario(String cpf, String nome, String email, String endereco) {
+		this.cpf = cpf;
+		this.nome = nome;
+		this.email = email;
+		this.endereco = endereco;
+	}
+	public String getCpf() {
+		return this.cpf;
+	}
+	public String getNome() {
+		return this.nome;
+	}
+	public String getEmail() {
+		return this.email;
+	}
+	public String getEndereco() {
+		return this.endereco;
+	}
+}
