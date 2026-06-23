7a4fa94c2a0c2d58bfd35521a7f9f65316bd940d
MENOB <senjumenob@gmail.com>
2026-06-23 18:09:09 -0300

    alteração em entidade Produto

--- src/main/java/com/upe/loja/repository/entity/Produto.java
+++ src/main/java/com/upe/loja/repository/entity/Produto.java
@@ -7,10 +7,10 @@ public class Produto {
     private BigDecimal taxaDiaria;
     private String conservacao;
     private BigDecimal valorReposicao;
-    private String estado;
+    private boolean estado;
 
     public Produto(String id, String nome, BigDecimal taxaDiaria,
-        String conservacao, BigDecimal valorReposicao, String estado){
+        String conservacao, BigDecimal valorReposicao, boolean estado){
 
         this.id = id;
         this.nome = nome;
@@ -26,10 +26,10 @@ public class Produto {
     public BigDecimal getTaxaDiaria(){ return this.taxaDiaria; }
     public String getConservacao(){ return this.conservacao; }
     public BigDecimal getValorReposicao(){ return this.valorReposicao; }
-    public String getEstado(){ return this.estado; }
+    public boolean getEstado(){ return this.estado; }
 
     //setters
-    public void setEstado(String estado){ this.estado = estado; }
+    public void setEstado(boolean estado){ this.estado = estado; }
     // Tireando ID, e Nome, o resto pode mudar.
     // TODO: Discutir com os amiguinhos sobre a relevancia de ter outros setters.
 }
