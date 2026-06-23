c15717edd38f0e9e16ad8f28a5328f658369072b
Esley Aury <esleyaury@gmail.com>
2026-06-22 21:22:42 -0300

    add: add carregar json to HashMap, listar produtos, salvar

--- src/main/java/com/upe/loja/repository/ProdutoRepository.java
+++ src/main/java/com/upe/loja/repository/ProdutoRepository.java
@@ -1,25 +1,60 @@
 package com.upe.loja.repository;
 import java.io.File;
 import java.io.IOException;
-import com.upe.loja.repository.entity.*;
+import com.upe.loja.repository.entity.Produto;
+import java.util.Map;
+import java.util.HashMap;
 
-//import com.fasterxml.jackson.core.JsonProcessingException;
+//Jackson JSON
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
 
 public class ProdutoRepository {
   private ObjectMapper mapper;
+  private Map<String, Produto> estoque;
   private Produto produto;
+  private final File arquivoDestino = new File("produtos.json");
 
   public ProdutoRepository(Produto produto){
     this.mapper = new ObjectMapper();
     this.produto = produto;
+    this.estoque = carregar();
   }
 
-  public void salvarProduto(){
-    try {
-      this.mapper.writerWithDefaultPrettyPrinter().writeValue(new File("produto.json"), this.produto);
-    } catch (IOException e){
+  public Map<String, Produto> carregar(){
+
+    try{
+      return mapper.readValue(arquivoDestino, new TypeReference<Map<String, Produto>>(){});
+    } catch (Exception e){
+      System.err.println(e.getMessage());
       e.printStackTrace();
     }
   }
+  public void salvar(Produto produto){
+    this.estoque = carregar();
+    estoque.put(produto.getID(), );
+    try {
+      File arquivoDestino = new File("produtos.json");
+
+      mapper.writerWithDefaultPrettyPrinter()
+        .writeValue(arquivoDestino, estoque);
+    }
+  }
+
+  public void listarTodos(){
+    this.estoque = carregar();
+    if (estoque.isEmpty()){
+      System.out.println("Nenhum produto encontrado!");
+    }
+
+    estoque.forEach((id, produto)->{System.out.println("ID: " +id +
+          " | Nome: " + produto.getNome() + " | Taxa Diaria " + produto.getTaxaDiaria() +
+          " | Conservacao: " + produto.getConservacao());
+    });
+
+  }
+
+  public Produto buscarProduto();
+  public void atualizar(Produto produto);
+  public void remover(String id)
 }
