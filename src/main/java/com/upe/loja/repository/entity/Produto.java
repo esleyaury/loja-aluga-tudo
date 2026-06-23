86ced597cb4036de735d2e0a1967a052f1fef864
Mateus Freitas <mateus.fgomes@upe.br>
2026-06-23 20:29:47 -0300

    release: setters de taxaDiaria, conservacao e valorReposicao criados

--- src/main/java/com/upe/loja/repository/entity/Produto.java
+++ src/main/java/com/upe/loja/repository/entity/Produto.java
@@ -23,13 +23,16 @@ public class Produto {
     //getters 
     public String getID(){ return this.id; }
     public String getNome(){ return this.nome; }
-    public BigDecimal getTaxaDiaria(){ return this.taxaDiaria; }
-    public String getConservacao(){ return this.conservacao; }
-    public BigDecimal getValorReposicao(){ return this.valorReposicao; }
-    public boolean getEstado(){ return this.estado; }
+    public BigDecimal getTaxaDiaria(){ return this.taxaDiaria;}
+    public String getConservacao(){ return this.conservacao;}
+    public BigDecimal getValorReposicao(){ return this.valorReposicao;}
+    public boolean getEstado(){ return this.estado;}
 
     //setters
-    public void setEstado(boolean estado){ this.estado = estado; }
+    public void setTaxaDiaria(BigDecimal taxaDiaria){ this.taxaDiaria = taxaDiaria;}
+    public void setConservacao(String conservacao){ this.conservacao = conservacao;}
+    public void setValorReposicao(BigDecimal valorReposicao){ this.valorReposicao = valorReposicao;}
+    public void setEstado(boolean estado){ this.estado = estado;}
     // Tireando ID, e Nome, o resto pode mudar.
     // TODO: Discutir com os amiguinhos sobre a relevancia de ter outros setters.
 }
