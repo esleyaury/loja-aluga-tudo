160a06f0ec6849cb81bbd625388b1b42f9cc8c11
Mateus Freitas <mateus.fgomes@upe.br>
2026-06-23 20:27:33 -0300

    release: assinatura de verificarDisponibilidade(String nome) criada

--- src/main/java/com/upe/loja/business/interfaces/ProdutoInterface.java
+++ src/main/java/com/upe/loja/business/interfaces/ProdutoInterface.java
@@ -1,11 +1,11 @@
 package com.upe.loja.business.interfaces;
+import java.util.List;
 
 import com.upe.loja.repository.ProdutoRepository;
 import com.upe.loja.repository.entity.Produto;
 
 public interface ProdutoInterface {
 
-    public Produto verificarDisponibilidade(ProdutoRepository produto);
-
+    public List<Produto> verificarDisponibilidade(String nome);
 
 }
