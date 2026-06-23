3b373f006ed0b981fdfd660568b28f59c0158d35
MENOB <senjumenob@gmail.com>
2026-06-23 18:11:48 -0300

    ajustes em ProdutoBusiness

--- src/main/java/com/upe/loja/business/ProdutoBusiness.java
+++ src/main/java/com/upe/loja/business/ProdutoBusiness.java
@@ -1,14 +1,25 @@
 package com.upe.loja.business;
+import com.upe.loja.business.interfaces.ProdutoInterface;
+import com.upe.loja.repository.ProdutoRepository;
 import com.upe.loja.repository.entity.Produto;
 import java.util.List;
 import java.util.ArrayList;
 
-public class ProdutoBusiness{
-  private List<Produto> produtos;
+public class ProdutoBusiness implements ProdutoInterface{
   private int quantidadePedido;
+  private ProdutoRepository produto;
 
   public ProdutoBusiness(int quantidadePedido){
-    this.produto = new ArrayList<>();
     this.quantidadePedido = quantidadePedido;
+    this.produto = new ProdutoRepository();
   }
+
+
+  @Override
+    public Produto verificarDisponibilidade(ProdutoRepository produto){
+      
+    }
+
+  
+
 }
