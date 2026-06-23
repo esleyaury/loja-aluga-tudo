17fa6681330dbd609b85ba2591495464f9caf0c1
MENOB <senjumenob@gmail.com>
2026-06-23 18:08:30 -0300

    exclusão de antiga interface

--- src/main/java/com/upe/loja/business/interfaces/AluguelInterface.java
+++
@@ -1,7 +0,0 @@
-package com.upe.loja.business.interfaces;
-
-public interface AluguelInterface{
-  String verificarDisponibilidade(Produto produto);
-  BigDecimal calcularValor(Produto produto);
-  boolean verificarInadimplencia(Cliente cliente);
-}
