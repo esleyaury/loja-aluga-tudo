2e702af6874386a14789f2591f946c51b3053ef8
Esley Aury <esleyaury@gmail.com>
2026-06-24 11:32:42 -0300

    update(ProdutoBusiness): corrigido Enum e vazamento de camada
    - A comparacao mudou de equals(Disponivel) para == Produto.EstadoProduto.DISPONIVEL
    - Removido println, apenas retornando uma lista vazia em caso do listaEstoque ser vazio.

--- src/main/java/com/upe/loja/business/ProdutoBusiness.java
+++ src/main/java/com/upe/loja/business/ProdutoBusiness.java
@@ -12,24 +12,21 @@ public class ProdutoBusiness implements ProdutoInterface{
     this.estoque = repository; 
   }
 
-
   @Override
     public List<Produto> verificarDisponibilidade(String nome){
       List<Produto> listaEstoque = this.estoque.buscarProduto(nome);
 
       if (listaEstoque.isEmpty()){
-        System.out.println("Nenhum produto encontrado!");//print em business
         return new ArrayList<>();
       }
 
       List<Produto> disponiveis = new ArrayList<>();
 
       for (Produto produto : listaEstoque){
-        if (produto.getEstado().equals("DISPONIVEL")){ //ignorar case e acento ver isso
+        if (produto.getEstado() == Produto.EstadoProduto.DISPONIVEL){//ignorar case e acento ver isso
           disponiveis.add(produto);
         }
       }
-
       return disponiveis;
     }
 }
