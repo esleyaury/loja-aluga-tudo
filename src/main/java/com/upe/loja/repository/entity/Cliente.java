f91cd9fa904d14743de69dd2f86a031998408364
MENOB <senjumenob@gmail.com>
2026-06-24 03:09:53 -0300

    update: add contrutor vazio

--- src/main/java/com/upe/loja/repository/entity/Cliente.java
+++ src/main/java/com/upe/loja/repository/entity/Cliente.java
@@ -3,6 +3,8 @@ package com.upe.loja.repository.entity;
 public class Cliente extends Usuario{
     //atributos diferentes?
 
+    public Cliente(){} //construtor vazio p o JSON
+
     public Cliente(long cpf, String senha, String nome, long telefone, String email){
         super(cpf, senha, nome, telefone, email);
         //se add atributos colocar o this.xx = xx normal aqui
