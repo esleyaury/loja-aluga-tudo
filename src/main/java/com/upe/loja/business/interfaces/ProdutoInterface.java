8cd49f865435f52f8469506e17f836df2b7b662c
Esley Aury <esleyaury@gmail.com>
2026-06-24 12:34:25 -0300

    add(ProdutoInterface): adicionar comentarios sobre futuros metodos a serem implementados
    - salvar produto
    - atualizar algum componente do Produto
    - listar todos os produtos (Funcao ja feita em ProdutoRepository)
    - remover( ja feito em ProdutoRepository)

--- src/main/java/com/upe/loja/business/interfaces/ProdutoInterface.java
+++ src/main/java/com/upe/loja/business/interfaces/ProdutoInterface.java
@@ -1,11 +1,12 @@
 package com.upe.loja.business.interfaces;
 import java.util.List;
 
-import com.upe.loja.repository.ProdutoRepository;
 import com.upe.loja.repository.entity.Produto;
 
 public interface ProdutoInterface {
-
     public List<Produto> verificarDisponibilidade(String nome);
-
+    // void salvar(Produto produto);
+    // void atualizar(Produto produto, int option, String valor);
+    // List<Produto> listarTodos();
+    // void remover(String id);
 }
