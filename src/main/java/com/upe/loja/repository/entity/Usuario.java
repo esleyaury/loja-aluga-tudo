22607ca505db6d535643ac240b6593f926223074
MENOB <senjumenob@gmail.com>
2026-06-24 02:35:49 -0300

    release: criação da base da entidade Usuario

---
+++ src/main/java/com/upe/loja/repository/entity/Usuario.java
@@ -0,0 +1,50 @@
+package com.upe.loja.repository.entity;
+
+public class Usuario {
+    private long cpf;
+    private String senha; // um cpf pode ser cliente e funcionario, por exemplo, oq vai diferenciar o acesso vai ser essa senha
+    private String nome;
+    private long telefone;
+    private String email;
+
+    public Usuario(){} // construtor vazio para o JSON
+
+    public Usuario(long cpf, String senha, String nome, long telefone, String email){
+        this.cpf = cpf;
+        this.senha = senha;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+
+    }
+
+    public long getCpf(){
+        return this.cpf;
+    }
+
+    public String getSenha(){
+        return this.senha;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public long getTelefone(){
+        return this.telefone;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    //setters
+    //os outros acho desnecessario a gente incluir ao menos inicialmente, vamos focar no básico
+
+    public String setSenha(String senha){return this.senha = senha;}
+
+    public long setTelefone(long telefone){return this.telefone = telefone;}
+
+    public String setEmail(String email){return this.email = email;}
+
+}
