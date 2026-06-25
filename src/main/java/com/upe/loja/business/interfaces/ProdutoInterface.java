0a6332866d0ffdd55f4a2bfef95a1d9f107fb740
MENOB <senjumenob@gmail.com>
2026-06-24 22:12:54 -0300

    update(ProdutoInterface): adicionando novas assinaturas de métodos

--- src/main/java/com/upe/loja/business/interfaces/ProdutoInterface.java
+++ src/main/java/com/upe/loja/business/interfaces/ProdutoInterface.java
@@ -5,8 +5,8 @@ import com.upe.loja.repository.entity.Produto;
 
 public interface ProdutoInterface {
     public List<Produto> verificarDisponibilidade(String nome);
-    // void salvar(Produto produto);
-    // void atualizar(Produto produto, int option, String valor);
-    // List<Produto> listarTodos();
-    // void remover(String id);
+    void salvar(Produto produto);
+    void atualizar(Produto produto, int option, String valor);
+    List<Produto> listarTodos();
+    void remover(String id);
 }
