89ff9710f1274cb2ac054d15e7267cb5bca238df
Aury <79032896+esleyaury@users.noreply.github.com>
2026-06-25 12:30:18 -0300

    create(Facade): criado a classe Facade

---
+++ src/main/java/com/upe/loja/Facade.java
@@ -0,0 +1,18 @@
+package com.upe.loja;
+import com.upe.loja.business.*;
+import com.upe.loja.repository.entity.*;;
+
+public class Facade {
+
+    public void cadastrarProduto(){
+        // Todo o processo de cadastrar um produto.
+        // Criar um novo produto
+        // settar as informaçoes dele
+        // salvar nele no csv
+    }
+
+    // Isso vai precisar retornar 
+    public void
+
+    
+}
