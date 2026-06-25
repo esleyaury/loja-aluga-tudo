732a98eecd5cfe234a04374340c167107ec918a4
MENOB <senjumenob@gmail.com>
2026-06-25 17:56:23 -0300

    Update(Facade): Adicionando métodos de ProdutoBusiness em Facade

--- src/main/java/com/upe/loja/Facade.java
+++ src/main/java/com/upe/loja/Facade.java
@@ -1,18 +1,47 @@
 package com.upe.loja;
+import java.math.BigDecimal;
+import java.util.List;
+
 import com.upe.loja.business.*;
-import com.upe.loja.repository.entity.*;;
+import com.upe.loja.repository.entity.*;
 
 public class Facade {
+    private final ProdutoBusiness produtoBusiness;
 
-    public void cadastrarProduto(){
+    public Facade (ProdutoBusiness produtoBusiness){
+        this.produtoBusiness = produtoBusiness;
+    }
+
+    public void cadastrarProduto(String id, String nome, BigDecimal taxaDiaria,
+        String conservacao, BigDecimal valorReposicao, Produto.EstadoProduto estado){
         // Todo o processo de cadastrar um produto.
         // Criar um novo produto
+        Produto produto = new Produto(id, nome, taxaDiaria, conservacao, valorReposicao, estado);
+        produtoBusiness.salvar(produto);
         // settar as informaçoes dele
         // salvar nele no csv
     }
 
     // Isso vai precisar retornar 
-    public void
+    public List<Produto> verificarDisponibilidade(String nome){
+        return produtoBusiness.verificarDisponibilidade(nome);
+    }
 
+    public void atualizarProduto(Produto produto, int option, String valor){
+        produtoBusiness.atualizar(produto, option, valor);
+    }
+
+    public List<Produto> listarTodos(){
+        return produtoBusiness.listarTodos();
+    }
+
+    public void removerProduto(String id){
+        produtoBusiness.remover(id);
+    }
+
+    public void fecharPrograma(){
+        produtoBusiness.guardarDados(); 
+        //se tiver fechamento de mais algo coloca aq junto
+    }
     
 }
