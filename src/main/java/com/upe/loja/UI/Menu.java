4058245736dec8c74fb0ab444cbf16a6664667b2
Aury <79032896+esleyaury@users.noreply.github.com>
2026-06-25 12:17:35 -0300

    update(Menu.java): adicionado a logica de menu
    - Scanner dentro da classe Menu
    - Estrutura pronta para receber o FACADE dentro de seu respectiv case, no switch dentro de processarOpcao()

--- src/main/java/com/upe/loja/UI/Menu.java
+++ src/main/java/com/upe/loja/UI/Menu.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class Menu{
     private Scanner entrada;
+    private int opc;
 
     public Menu(Scanner entrada){
-        this.entrada = entrada;
+        this.entrada = new Scanner(System.in);
+        this.opc = -1;
     }
 
     public void iniciar(){
-        int opc = -1;
         
         do{
             exibirOpcoes();
             String digitado = entrada.nextLine();
-
             try{
                 opc = Integer.parseInt(digitado);
                 processarOpcao(opc);
@@ -25,9 +25,33 @@ public class Menu{
         } while (opc != 0) ;
     }
     private void exibirOpcoes(){
-        System.out.println("Menu da Loja MENOB's");
-        System.out.print("Escolha uma opção: ");
+        System.out.println("------ Menu da Loja MENOB's -----");
+        System.out.println("Escolha uma opção: ");
+        System.out.println("1 - Cadastrar");
+        System.out.println("2 - Listar Produtos");
+        System.out.println("3 - Atualizar Produto");
+        System.out.println("4 - Remover");
+        System.out.println("0 - Sair");
+        System.out.println("Digite sua escolha abaixo: ");
     }
     public void processarOpcao(int opc){
+        switch(opc){
+            case 1:
+                // Cadastrar novo produto
+                // Instancia um novo produto, e salva ele
+                break;
+            case 2:
+                // ListarTodos();
+                break;
+            case 3:
+                //Atualizar o produto, e salvar ele
+                break;
+            case 4:
+                // Remove o produto, e precisa salvar isso.
+            case 0:
+                break;
+            default:
+                System.out.println("Opção Invalida!!!");
         }
+    }
 }
