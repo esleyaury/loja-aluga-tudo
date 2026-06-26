f59ebaf9ccdeb735e1aab779a4b642c342daa566
MENOB <senjumenob@gmail.com>
2026-06-25 23:15:15 -0300

    Update(ProdutoInterface): Alteração de assinaturas de interface

--- src/main/java/com/upe/loja/business/interfaces/ProdutoInterface.java
+++ src/main/java/com/upe/loja/business/interfaces/ProdutoInterface.java
@@ -1,5 +1,6 @@
 package com.upe.loja.business.interfaces;
 import java.util.List;
+import java.util.Map;
 
 import com.upe.loja.repository.entity.Produto;
 
@@ -7,7 +8,7 @@ public interface ProdutoInterface {
     public List<Produto> verificarDisponibilidade(String nome);
     void salvar(Produto produto);
     void atualizar(Produto produto, int option, String valor);
-    List<Produto> listarTodos();
+    Map<String, Produto> listarTodos();
     void remover(String id);
     void guardarDados();
 }
