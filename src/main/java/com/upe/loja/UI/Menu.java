d2d4a71d6a40a3df137ff8f11b2b86d79d556013
MENOB <senjumenob@gmail.com>
2026-06-25 23:16:53 -0300

    Update(Menu): Alteração de menu e ajustes de métodos existentes

--- src/main/java/com/upe/loja/UI/Menu.java
+++ src/main/java/com/upe/loja/UI/Menu.java
@@ -1,17 +1,21 @@
 package com.upe.loja.UI;
 import com.upe.loja.Facade;
+import com.upe.loja.repository.entity.Produto;
 
 import java.util.Scanner;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
 
 public class Menu{
     private Scanner entrada;
     private int opc;
     private Facade facade;
 
-    public Menu(Scanner entrada){
+    public Menu(Facade facade){
         this.entrada = new Scanner(System.in);
         this.opc = -1;
-        this.facade = new Facade();
+        this.facade = facade;
     }
 
     public void iniciar(){
@@ -47,14 +51,17 @@ public class Menu{
                 menuCadastrarProduto();
                 break;
             case 2:
-                // ListarTodos();
+                menuListarProdutos();
                 break;
             case 3:
-                //Atualizar o produto, e salvar ele
+                menuAtualizarProduto();
                 break;
             case 4:
-                // Remove o produto, e precisa salvar isso.
+                menuRemoverProduto();
+                break;
+
             case 0:
+                facade.fecharPrograma();
                 break;
             default:
                 System.out.println("Opção Invalida!!!");
@@ -62,14 +69,47 @@ public class Menu{
     }
 
     public void menuCadastrarProduto(){
-      System.out.println("Digite: nome, taxaDiaria, conservacao, valorReposicao");
+      System.out.println("Digite: id, nome, taxaDiaria, conservacao, valorReposicao");
       Scanner scanner = new Scanner(System.in);
       String linha = scanner.nextLine();
+      //scanner.close();
       String[] partes = linha.split(",");
-      String nome = partes[0].trim();
-      BigDecimal taxaDiaria = new BigDecimal(partes[1].trim());
-      String conservacao = partes[2].trim();
-      BigDecimal valorReposicao = new BigDecimal(partes[3].trim());
-      facade.cadastrarProduto(nome, taxaDiaria, conservacao, valorReposicao);
+      String id = partes[0].trim();
+      String nome = partes[1].trim();
+      BigDecimal taxaDiaria = new BigDecimal(partes[2].trim());
+      String conservacao = partes[3].trim();
+      BigDecimal valorReposicao = new BigDecimal(partes[4].trim());
+      facade.cadastrarProduto(id, nome, taxaDiaria, conservacao, valorReposicao);
+    }
+
+    public void menuListarProdutos(){
+        Map<String, Produto> produtos = facade.listarTodos();
+        for (Produto p : produtos.values()){
+            System.out.println(p);
+        }
+    }
+
+    public void menuAtualizarProduto(){
+        System.out.println("Digite o ID do produto que deseja atualizar:\n");
+        Scanner scanner = new Scanner (System.in);
+        String id = scanner.nextLine();
+        Produto produtoEncontrado = facade.buscarPorId(id);
+        System.out.println("O que deseja alterar? \n 1- NOME 2-TAXA 3-CONSERVACAO 4-ESTADO\n");
+        int option = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("O que deseja inserir no lugar?\n");
+        String valor = scanner.nextLine();
+        //scanner.close();
+        facade.atualizarProduto(produtoEncontrado, option, valor);
+    }
+
+    public void menuRemoverProduto(){
+        System.out.println("Digite o ID do produto que deseja deletar:\n");
+        Scanner scanner = new Scanner(System.in);
+        String id = scanner.nextLine();
+        //scanner.close();
+        Produto produtoEncontrado = facade.buscarPorId(id);
+        String idProduto = produtoEncontrado.getID();
+        facade.removerProduto(idProduto);
     }
 }
