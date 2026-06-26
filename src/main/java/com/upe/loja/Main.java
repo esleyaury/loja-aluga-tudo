2c71054d29a8fdbd6fdf659b4ec9063fe79b3c5f
MENOB <senjumenob@gmail.com>
2026-06-25 23:18:29 -0300

    Update(Main):Atualizando inicialização da Main e do menu()

--- src/main/java/com/upe/loja/Main.java
+++ src/main/java/com/upe/loja/Main.java
@@ -1,10 +1,15 @@
 package com.upe.loja;
 
 import com.upe.loja.UI.*;
+import com.upe.loja.business.ProdutoBusiness;
+import com.upe.loja.repository.ProdutoRepository;
 public class Main{
   public static void main(String[] args) {  
+    ProdutoRepository repository = new ProdutoRepository();
+    ProdutoBusiness business = new ProdutoBusiness(repository);
+    Facade facade = new Facade(business);
 
-    Menu menu = new Menu(null);
+    Menu menu = new Menu(facade);
     menu.iniciar();
 
   }
