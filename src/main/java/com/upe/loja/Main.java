06b6ec611ff36c3d97284612dc74f600d9238843
MENOB <senjumenob@gmail.com>
2026-06-27 13:50:55 -0300

    Delete(All): Apagando arquivos não relacionados à clientes

--- src/main/java/com/upe/loja/Main.java
+++ src/main/java/com/upe/loja/Main.java
@@ -1,14 +1,6 @@
 package com.upe.loja;
-import com.upe.loja.repository.entity.*;
-import java.math.BigDecimal;
 
 public class Main{
-  public static void main(String[] args) {
-    Produto produto = new Produto("b52400", "Menob", new BigDecimal("22.2"), "Ben doente", new BigDecimal("0.0"), "Em manutenzion");
-    ProdutoRepository produtoEstoque = new ProdutoRepository(produto);
 
-    produtoEstoque.salvarProduto();
-    produtoEstoque.lerProduto();
-  }
 }
 
--- src/main/java/com/upe/loja/repository/entity/Cliente.java
+++ src/main/java/com/upe/loja/repository/entity/Cliente.java
@@ -3,8 +3,6 @@ package com.upe.loja.repository.entity;
 public class Cliente extends Usuario{
     //atributos diferentes?
 
-    public Cliente(){} //construtor vazio p o JSON
-
     public Cliente(long cpf, String senha, String nome, long telefone, String email){
         super(cpf, senha, nome, telefone, email);
         //se add atributos colocar o this.xx = xx normal aqui
