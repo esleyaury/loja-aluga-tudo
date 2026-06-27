49b737391c68ec6488e8f159dc788f7eb67acee1
MENOB <senjumenob@gmail.com>
2026-06-27 18:07:34 -0300

    Update(Main): Criação da Main em categoria

--- src/main/java/com/upe/loja/Main.java
+++ src/main/java/com/upe/loja/Main.java
@@ -1,7 +1,17 @@
 package com.upe.loja;
 
+import com.upe.loja.UI.MenuCategoria;
+import com.upe.loja.business.CategoriaBusiness;
+import com.upe.loja.repository.CategoriaRepository;
+
 public class Main{
   public static void main(String[] args) {
+    CategoriaRepository categoriaRepository= new CategoriaRepository();
+    CategoriaBusiness categoriaBusiness = new CategoriaBusiness(categoriaRepository);
+    Facade facade = new Facade(categoriaBusiness);
+
+    MenuCategoria menuCategoria = new MenuCategoria(facade);
+    menuCategoria.iniciar();
   }
 }
 
