d108dc65a66db9db00c9f516c7e1e9f43210f9d3
MENOB <senjumenob@gmail.com>
2026-06-27 14:26:33 -0300

    Update: Alteração de CategoriaRepository: - Criação de interface de repository - Criação de gerir arquivo de categorias - Alteração de CategoriaRepository

--- src/main/java/com/upe/loja/repository/CategoriaRepository.java
+++ src/main/java/com/upe/loja/repository/CategoriaRepository.java
@@ -1,69 +1,43 @@
 package com.upe.loja.repository;
 
-import com.upe.loja.repository.entity.Categoria;
-
 import java.util.HashSet;
 import java.util.Set;
 import java.io.File;
-import java.util.Collections;
-
-//Jackson JSON
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-
 
 public class CategoriaRepository {
 
-    private Set<String> categorias = new HashSet<>(); //usando hashset ja que so tem a string nome de categoria, ai n precisa ser hashmap
-    private ObjectMapper mapper;
-    private final File ARQUIVO_JSON = new File("categorias.json");
+    private Set<String> categorias;
+    private GerirCategoriasCSV gerenciadorArquivo;
+    private File arquivoCategorias;
 
     public CategoriaRepository(){
-    this.mapper = new ObjectMapper();
-    this.mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-    this.categorias = carregar();
+        this.arquivoCategorias = new File("categorias.csv");
+        this.gerenciadorArquivo = new GerirCategoriasCSV();
+        this.categorias = gerenciadorArquivo.carregar(this.arquivoCategorias);
     }
 
-    public Set<String> carregar() {
-        // lê o arquivo JSON e popula o HashMap
-        // (usando Gson ou Jackson)
-        try{
-            Set<String> objetoCarregado = new HashSet<>();
-            objetoCarregado = mapper.readValue(ARQUIVO_JSON, new TypeReference<Set<String>>(){});
-            return objetoCarregado;
-        } catch (Exception e){
-            System.err.println(e.getMessage());
-            e.printStackTrace();
-            return new HashSet<>();
-        }
-    }
-
-    // salvar() só escreve no JSON (chamado ao encerrar o programa)
-    public void salvar() {
-        try {
-            mapper.writerWithDefaultPrettyPrinter()
-            .writeValue(ARQUIVO_JSON, categorias);
-        } catch (Exception e) {
-            System.err.println(e);
-        }
-    }
-
-    // CRUD
-    public void adicionar(Categoria categoria) {
-        categorias.add(categoria.getNomeCategoria());
+    public void salvar(String nomeCategoria) {
+        categorias.add(nomeCategoria.toLowerCase());
     }
 
     public boolean buscarCategoria(String nomeCategoria) {
-        return categorias.contains(nomeCategoria);
+        return categorias.contains(nomeCategoria.toLowerCase());
     }
 
     public Set<String> listarCategorias() {
-        return Collections.unmodifiableSet(categorias);
+        return new HashSet<>(categorias);
     }
 
-    public void deletar(String nomeCategoria) {
-        categorias.remove(nomeCategoria); //nativo do hashset
+    public void remover(String nomeCategoria) {
+        categorias.remove(nomeCategoria.toLowerCase());
+    }
+
+    public void atualizar(String nomeAtual, String novoNome){
+        categorias.remove(nomeAtual.toLowerCase());
+        categorias.add(novoNome.toLowerCase());
+    }
+
+    public void guardarDados(){
+        gerenciadorArquivo.guardarDados(this.arquivoCategorias, this.categorias);
     }
 }
