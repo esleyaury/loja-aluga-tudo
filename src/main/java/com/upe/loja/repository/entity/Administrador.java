ff0e4a473e883d5db45c81e7c1417cd0700e5ca9
MENOB <senjumenob@gmail.com>
2026-06-27 13:55:15 -0300

    Delete(All): Apagando arquivos não relacionados à adm

--- src/main/java/com/upe/loja/repository/entity/Administrador.java
+++ src/main/java/com/upe/loja/repository/entity/Administrador.java
@@ -4,8 +4,6 @@ public class Administrador extends Usuario{
     
     //nao sei se tem algum atributo diferente
 
-    public Administrador(){} //construtor vazio p JSON
-
     public Administrador(long cpf, String senha, String nome, long telefone, String email){
         super(cpf, senha, nome, telefone, email);
         //se add atributo ai faz o this.xx = xx normal
