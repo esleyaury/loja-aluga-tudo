3a8812800f8d47afd146876d0d818bc9a65b00b9
MENOB <senjumenob@gmail.com>
2026-06-28 14:48:31 -0300

    Release(Facade): Criação de Facade em Funcionario

---
+++ src/main/java/com/upe/loja/Facade.java
@@ -0,0 +1,45 @@
+package com.upe.loja;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+import com.upe.loja.business.FuncionarioBusiness;
+import com.upe.loja.business.interfaces.IFuncionarioBusiness;
+import com.upe.loja.repository.entity.Funcionario;
+import com.upe.loja.repository.entity.Funcionario.Cargo;
+
+public class Facade {
+    private final IFuncionarioBusiness funcionarioBusiness;
+
+    public Facade(){
+        this.funcionarioBusiness = new FuncionarioBusiness();
+    }
+
+    public void cadastrarFuncionario(String id, String cpf, String senha, String nome, String email, BigDecimal salario, Cargo cargo){
+        funcionarioBusiness.cadastrarFuncionario(id, cpf, senha, nome, email, salario, cargo);
+    }
+
+    public Funcionario buscarPorId(String id){
+        return funcionarioBusiness.buscarPorId(id);
+    }
+    
+    public Funcionario buscarPorCpf(String cpf){
+        return funcionarioBusiness.buscarPorCpf(cpf);
+    }
+
+    public Map<String, Funcionario> listarTodos(){
+        return funcionarioBusiness.listarTodos();
+    }
+
+    public void atualizar(Funcionario funcionario, int option, String valor){
+        funcionarioBusiness.atualizar(funcionario, option, valor);
+    }
+
+    public void remover(String id){
+        funcionarioBusiness.remover(id);
+    }
+
+    public void guardarDados(){
+        funcionarioBusiness.guardarDados();
+    }
+}
