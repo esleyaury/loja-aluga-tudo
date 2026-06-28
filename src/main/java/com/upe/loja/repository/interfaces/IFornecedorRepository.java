c4821bf970715e2d8947a0c2ae51c5959f66b696
MENOB <senjumenob@gmail.com>
2026-06-28 20:57:36 -0300

    Update(IFornecedorRepository): Ajuste nos parametros de salvar()

--- src/main/java/com/upe/loja/repository/interfaces/IFornecedorRepository.java
+++ src/main/java/com/upe/loja/repository/interfaces/IFornecedorRepository.java
@@ -4,7 +4,7 @@ import java.util.Map;
 import com.upe.loja.repository.entity.*;;
 
 public interface IFornecedorRepository {
-    public void salvar();
+    public void salvar(Fornecedor fornecedor);
     public Map<String, Fornecedor> listarTodos();
     public Fornecedor buscarPorCnpj(String cnpj);
     public void atualizar(Fornecedor fornecedor);
