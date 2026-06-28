fd3ec2db20c413882181877c5216cf84e8d6286f
MENOB <senjumenob@gmail.com>
2026-06-28 20:49:09 -0300

    Release(IFornecedorRepository): Criação de interface para FornecedorRepository

--- src/main/java/com/upe/loja/repository/interfaces/IFornecedorRepository.java
+++ src/main/java/com/upe/loja/repository/interfaces/IFornecedorRepository.java
@@ -1,5 +1,13 @@
 package com.upe.loja.repository.interfaces;
 
-public interface IFornecedorRepository {
+import java.util.Map;
+import com.upe.loja.repository.entity.*;;
 
+public interface IFornecedorRepository {
+    public void salvar();
+    public Map<String, Fornecedor> listarTodos();
+    public Fornecedor buscarPorCnpj(String cnpj);
+    public void atualizar(Fornecedor fornecedor);
+    public void remover(String cnpj);
+    public void guardarDados();
 }
