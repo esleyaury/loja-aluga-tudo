356f49812868518b5fd461c800507ef9e733982a
Esley Aury <esleyaury@gmail.com>
2026-06-27 23:55:05 -0300

    update(Main): estabelicade preservacao de camada.
    - Removido instanciamento de outras camadas na main
    - A main agora conhece somente a camada de UI e o Facade

--- src/main/java/com/upe/loja/Main.java
+++ src/main/java/com/upe/loja/Main.java
@@ -1,17 +1,12 @@
 package com.upe.loja;
 
-import com.upe.loja.UI.*;
-import com.upe.loja.business.ProdutoBusiness;
-import com.upe.loja.repository.ProdutoRepository;
+import com.upe.loja.UI.Menu;
+
 public class Main{
-  public static void main(String[] args) {  
-    ProdutoRepository repository = new ProdutoRepository();
-    ProdutoBusiness business = new ProdutoBusiness(repository);
-    Facade facade = new Facade(business);
+  public static void main(String[] args) {
+    Facade facade = new Facade();
 
     Menu menu = new Menu(facade);
     menu.iniciar();
-
   }
 }
-
