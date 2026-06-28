d4134024208974c12acd2e2699ae8de4ddc36cc3
Esley Aury <esleyaury@gmail.com>
2026-06-28 09:35:43 -0300

    update(IProdutoRepository): Removido a responsabilidade do Facade de iniciar produto. adiocinado o metodo buscarPorId(), que estava faltando na Interface

--- src/main/java/com/upe/loja/business/interfaces/IProdutoBusiness.java
+++ src/main/java/com/upe/loja/business/interfaces/IProdutoBusiness.java
@@ -6,14 +6,9 @@ import java.util.Map;
 import com.upe.loja.repository.entity.Produto;
 
 public interface IProdutoBusiness {
-    // [CORREÇÃO ANTIGRAVITY] Adicionado método cadastrarProduto que recebe dados
-    // primitivos. A criação do objeto Produto é responsabilidade da camada de
-    // negócio, não da Facade.
     void cadastrarProduto(String id, String nome, BigDecimal taxaDiaria,
         String conservacao, BigDecimal valorReposicao);
     public List<Produto> verificarDisponibilidade(String nome);
-    // [CORREÇÃO ANTIGRAVITY] Método buscarPorId estava faltando na interface
-    // embora fosse implementado no ProdutoBusiness e usado pelo Facade.
     Produto buscarPorId(String id);
     void salvar(Produto produto);
     void atualizar(Produto produto, int option, String valor);
