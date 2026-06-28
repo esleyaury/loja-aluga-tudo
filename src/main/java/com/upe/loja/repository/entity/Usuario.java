2601ea5bbb135de63f9ef84d58e7bf66d7e9fea9
Mateus Freitas <mateus.fgomes@upe.br>
2026-06-27 22:33:28 -0300

    release: Estrutura de Usuario montada

--- src/main/java/com/upe/loja/repository/entity/Usuario.java
+++ src/main/java/com/upe/loja/repository/entity/Usuario.java
@@ -1,37 +1,29 @@
 package com.upe.loja.repository.entity;
 
-public class Usuario {
+public abstract class Usuario {
+    private String id;
     private long cpf;
-    private String senha; // um cpf pode ser cliente e funcionario, por exemplo, oq vai diferenciar o acesso vai ser essa senha
     private String nome;
-    private long telefone;
     private String email;
 
-    public Usuario(){} // construtor vazio para o JSON
-
-    public Usuario(long cpf, String senha, String nome, long telefone, String email){
+    
+    public Usuario(String id, long cpf, String nome, String email){
+        this.id = id;
         this.cpf = cpf;
-        this.senha = senha;
         this.nome = nome;
-        this.telefone = telefone;
         this.email = email;
+    }
 
+    public String getId(){
+        return this.id;
     }
 
     public long getCpf(){
         return this.cpf;
     }
 
-    public String getSenha(){
-        return this.senha;
-    }
-
     public String getNome(){
         return this.nome;
-    }
-
-    public long getTelefone(){
-        return this.telefone;
     }
 
     public String getEmail(){
@@ -40,11 +32,8 @@ public class Usuario {
 
     //setters
     //os outros acho desnecessario a gente incluir ao menos inicialmente, vamos focar no básico
-
-    public String setSenha(String senha){return this.senha = senha;}
-
-    public long setTelefone(long telefone){return this.telefone = telefone;}
-
+    public long setCpf(long cpf){return this.cpf = cpf;}
+    public String setNome(String nome){return this.nome = nome;}
     public String setEmail(String email){return this.email = email;}
 
 }
