75d19186704d86f1d96249ac45daf74fffed5ed2
MENOB <senjumenob@gmail.com>
2026-06-28 16:18:35 -0300

    Update(IFuncionarioBusiness): Alterando parametro de atualizar()

--- src/main/java/com/upe/loja/business/interfaces/IFuncionarioBusiness.java
+++ src/main/java/com/upe/loja/business/interfaces/IFuncionarioBusiness.java
@@ -8,11 +8,11 @@ import java.util.Map;
 
 public interface IFuncionarioBusiness {
     public void cadastrarFuncionario(String id, String cpf, String senha, String nome, String email, BigDecimal salario, Cargo cargo);
-    public void salvar(Funcionario funcionario);
+    public void salvar(String id);
     public Funcionario buscarPorId(String id);
     public Funcionario buscarPorCpf(String cpf);
     public Map<String, Funcionario> listarTodos();
-    public void atualizar(Funcionario funcionario, int option, String valor);
+    public void atualizar(String id, int option, String valor);
     public void remover(String id);
     public void guardarDados();
 }
