2ab0f4433330cfb2b95d2d6732244c657c1d7074
MENOB <senjumenob@gmail.com>
2026-06-28 16:39:10 -0300

    Release(Main): Iniciando Main em Funcionario

--- src/main/java/com/upe/loja/Main.java
+++ src/main/java/com/upe/loja/Main.java
@@ -1,8 +1,13 @@
 package com.upe.loja;
 
+import com.upe.loja.UI.MenuFuncionario;
+
 public class Main{
   public static void main(String[] args) {  
+    Facade facade = new Facade();
 
+    Menu menu = new Menu(Facade);
+    menu.iniciar();
   }
 }
 
