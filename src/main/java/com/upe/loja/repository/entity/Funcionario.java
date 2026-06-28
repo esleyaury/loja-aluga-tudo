519f1536d92628b76df37f2443c7ea41c8652ad4
Mateus Freitas <mateus.fgomes@upe.br>
2026-06-28 05:29:36 -0300

    Conflito resolvido

--- src/main/java/com/upe/loja/repository/entity/Funcionario.java
+++ src/main/java/com/upe/loja/repository/entity/Funcionario.java
@@ -1,16 +1,48 @@
 package com.upe.loja.repository.entity;
 
+import java.math.BigDecimal;
+
 public class Funcionario extends Usuario{
 
-    //vai ter atributos diferentes?
+    private Cargo cargo;
+    private BigDecimal salario;
 
-    public Funcionario(){} //construtor vazio p JSON
-
-    public Funcionario(long cpf, String senha, String nome, long telefone, String email){
-        super(cpf, senha, nome, telefone, email);
-        //se tiver atributos diferentes fazer o this.xx = xx normal
+    public Funcionario(String id, String cpf, String senha, String nome, String email, BigDecimal salario, Cargo cargo){
+        super(id,cpf,senha,nome,email,tipoPerfil.Funcionario);
+        this.salario = salario;
+        this.cargo = cargo;
     }
-
-    //getters e setters so se add algo?
-
+    public enum Cargo{
+        ESTAGIARIO,
+        ATENDENTE,
+        CAIXA,
+        GERENTE,
+        CEO
+    }
+    public BigDecimal getSalario(){
+        return this.salario;
+    }
+    public Cargo getCargo(){
+        return this.cargo;
+    }
+    public void setSalario(BigDecimal salarioNovo){
+        if (salarioNovo == null){
+            throw new IllegalArgumentException("O Salário é um campo obrigatório.");
+        }
+        if (salarioNovo.compareTo(new BigDecimal("200"))<0){
+            throw new IllegalArgumentException("Salário mínimo é R$ 200,00.");
+        }
+        /*verificação sobre o teto do salário
+        Estagiario: 200 -- 800
+        Atendente: 1500 -- 4000
+        Caixa: 1500 -- 4000
+        Gerente: 5000 -- 15000
+        Ceo: 15000 -- o céu é o limite
+        */
+       this.salario = salarioNovo;
+    }
+    public void setCargo(Cargo cargo){
+        this.cargo = cargo;
+        /*Adicionar verificação para CEO, só pode ter 3 em ativação*/
+    }
 }
