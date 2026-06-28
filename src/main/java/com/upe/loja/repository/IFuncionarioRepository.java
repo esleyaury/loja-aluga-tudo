3a3faf5cfc9fb469b0879da64e007e213941f48c
Mateus Freitas <mateus.fgomes@upe.br>
2026-06-28 06:17:21 -0300

    Assinaturas dos métodos criadas

---
+++ src/main/java/com/upe/loja/repository/IFuncionarioRepository.java
@@ -0,0 +1,14 @@
+package com.upe.loja.repository;
+
+import java.util.Map;
+import com.upe.loja.repository.entity.Funcionario;
+
+public interface IFuncionarioRepository {
+    public void salvar(Funcionario funcionario);
+    public Funcionario buscarPorId(String id);
+    public Funcionario buscarPorCpf(String cpf);
+    public Map<String, Funcionario> listarTodos();
+    public void atualizar(Funcionario funcionario);
+    public void remover(String id);
+    public void guardarDados();
+}
