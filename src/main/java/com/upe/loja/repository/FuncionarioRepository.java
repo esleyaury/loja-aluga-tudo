07f8895bef1344af275cf38510ee8f2b19ac2888
Mateus Freitas <mateus.fgomes@upe.br>
2026-06-28 12:14:16 -0300

    Classe FuncionarioRepository.java finalizada

---
+++ src/main/java/com/upe/loja/repository/FuncionarioRepository.java
@@ -0,0 +1,44 @@
+package com.upe.loja.repository;
+
+import com.upe.loja.repository.IFuncionarioRepository;
+import com.upe.loja.repository.entity.Funcionario;
+import com.upe.loja.repository.entity.Usuario;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.util.List;
+import java.util.Map;
+import java.io.File; 
+
+public class FuncionarioRepository implements IFuncionarioRepository{
+    private Map<String, Funcionario> funcionarios;
+    private GerirFuncionariosCSV gereciadorArquivo;
+    private File arquivoFuncionarios;
+
+    public FuncionarioRepository(){
+        this.arquivoFuncionarios = new File("funcionarios.csv");
+        this.gereciadorArquivo = new GerirFuncionariosCSV();
+        this.funcionarios = gereciadorArquivo.carregar(this.arquivoFuncionarios);
+    }
+    public void salvar(Funcionario funcionario){
+        funcionarios.put(funcionario.getId(), funcionario);
+    }
+    public Funcionario buscarPorId(String id){
+        return funcionarios.get(id);
+    }
+    public Map<String, Funcionario> listarTodos(){
+        return new HashMap<>(funcionarios);
+    }
+    public List<Funcionario> buscarPorCpf(String cpf){
+        return this.funcionarios.values().stream().filter(func -> func.getNome().equalsIgnoreCase(cpf.trim()))
+        .collect(Collectors.toList());
+    }
+    public void atualizar(Funcionario funcionario){
+        funcionarios.put(funcionario.getId(), funcionario);
+    }
+    public void remover(String id){
+        this.funcionarios.remove(id);
+    }
+    public void guardarDados(){
+        gereciadorArquivo.guardarDados(this.arquivoFuncionarios, this.funcionarios);
+    }
+}
