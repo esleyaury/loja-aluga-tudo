ed18b4218745597953e5edd69117417d8196baf7
Esley Aury <esleyaury@gmail.com>
2026-06-28 09:30:38 -0300

    update(Main): Agora a main, camada de UI, conhece somente o Facade

--- src/main/java/com/upe/loja/Main.java
+++ src/main/java/com/upe/loja/Main.java
@@ -2,10 +2,6 @@ package com.upe.loja;
 
 import com.upe.loja.UI.Menu;
 
-// [CORREÇÃO ANTIGRAVITY] Main simplificada: antes instanciava ProdutoRepository e
-// ProdutoBusiness diretamente, violando o requisito do projeto:
-// "A UI não deve instanciar regras de negócio ou repositórios diretamente."
-// Agora a Main (camada de apresentação) só conhece o Facade e o Menu.
 public class Main{
   public static void main(String[] args) {
     Facade facade = new Facade();
