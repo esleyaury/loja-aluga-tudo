4e3a2db888b9efa7479ff1cedd6535442ec3a877
MENOB <senjumenob@gmail.com>
2026-06-28 13:10:03 -0300

    Update(IFuncionarioBusiness): Adição de assinatura para cadastro de funcionário

--- src/main/java/com/upe/loja/business/interfaces/IFuncionarioBusiness.java
+++ src/main/java/com/upe/loja/business/interfaces/IFuncionarioBusiness.java
@@ -1,9 +1,13 @@
 package com.upe.loja.business.interfaces;
 
 import com.upe.loja.repository.entity.Funcionario;
+import com.upe.loja.repository.entity.Funcionario.Cargo;
+
+import java.math.BigDecimal;
 import java.util.Map;
 
 public interface IFuncionarioBusiness {
+    public void cadastrarFuncionario(String id, String cpf, String senha, String nome, String email, BigDecimal salario, Cargo cargo);
     public void salvar(Funcionario funcionario);
     public Funcionario buscarPorId(String id);
     public Funcionario buscarPorCpf(String cpf);
