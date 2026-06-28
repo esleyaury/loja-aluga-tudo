9808bbe6164231727dcd5293940da4f20cd5fbbd
MENOB <senjumenob@gmail.com>
2026-06-28 10:23:23 -0300

    Release(IProdutoBusiness): Criação de interface para FuncionarioBusiness

---
+++ src/main/java/com/upe/loja/business/interfaces/IFuncionarioBusiness.java
@@ -0,0 +1,14 @@
+package com.upe.loja.business.interfaces;
+
+import com.upe.loja.repository.entity.Funcionario;
+import java.util.Map;
+
+public interface IFuncionarioBusiness {
+    public void salvar(Funcionario funcionario);
+    public Funcionario buscarPorId(String id);
+    public Funcionario buscarPorCpf(String cpf);
+    public Map<String, Funcionario> listarTodos();
+    public void atualizar(Funcionario funcionario);
+    public void remover(String id);
+    public void guardarDados();
+}
