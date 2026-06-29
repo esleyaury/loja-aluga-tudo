553194ed6b70da5bfbd7d0c0abd58f166dbad92a
MENOB <senjumenob@gmail.com>
2026-06-28 21:28:03 -0300

    Release(FornecedorRepository): Criação de FornecedorRepository e seus métodos

--- src/main/java/com/upe/loja/repository/FornecedorRepository.java
+++ src/main/java/com/upe/loja/repository/FornecedorRepository.java
@@ -1,5 +1,38 @@
 package com.upe.loja.repository;
 
-public class FornecedorRepository {
+import java.util.HashMap;
+import java.util.Map;
+import java.io.File;
 
+import com.upe.loja.repository.entity.Fornecedor;
+import com.upe.loja.repository.interfaces.IFornecedorRepository;
+
+public class FornecedorRepository implements IFornecedorRepository{
+    private Map<String, Fornecedor> fornecedores;
+    private GerirFornecedoresCSV gerenciadorArquivo;
+    private File arquivoFornecedores;
+
+    public void salvar(Fornecedor fornecedor){
+        fornecedores.put(fornecedor.getCnpj(),fornecedor);
+    }
+
+    public Map<String, Fornecedor> listarTodos(){
+        return new HashMap<>(fornecedores);
+    }
+
+    public Fornecedor buscarPorCnpj(String cnpj){
+        return fornecedores.get(cnpj);
+    }
+
+    public void atualizar(Fornecedor fornecedor){
+        fornecedores.put(fornecedor.getCnpj(), fornecedor);
+    }
+    
+    public void remover(String cnpj){
+        this.fornecedores.remove(cnpj);
+    }
+    
+    public void guardarDados(){
+        gerenciadorArquivo.guardarDados(this.arquivoFornecedores, this.fornecedores);
+    }
 }
