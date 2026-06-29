a45f6f0973e938977990c755fe3344a643d534bc
MENOB <senjumenob@gmail.com>
2026-06-29 18:18:41 -0300

    Release(Main): Criando main funcional para operações no menu de fornecedor

--- src/main/java/com/upe/loja/Main.java
+++ src/main/java/com/upe/loja/Main.java
@@ -1,8 +1,13 @@
 package com.upe.loja;
 
+import com.upe.loja.UI.MenuFornecedor;
+
 public class Main{
   public static void main(String[] args) {  
+    Facade facade = new Facade();
 
+    MenuFornecedor menu = new MenuFornecedor(facade);
+    menu.iniciar();
   }
 }
 
