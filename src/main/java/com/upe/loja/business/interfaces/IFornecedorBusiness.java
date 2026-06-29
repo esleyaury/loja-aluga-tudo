9d84f52d56572d6eb2aaee8025bd1be6acc5aa2a
MENOB <senjumenob@gmail.com>
2026-06-29 16:38:18 -0300

    Release(IFornecedorBusiness): Criação de interface para FornecedorBusiness

--- src/main/java/com/upe/loja/business/interfaces/IFornecedorBusiness.java
+++ src/main/java/com/upe/loja/business/interfaces/IFornecedorBusiness.java
@@ -1,5 +1,16 @@
 package com.upe.loja.business.interfaces;
 
-public interface IFornecedorBusiness {
+import com.upe.loja.repository.entity.Fornecedor;
 
+import java.util.Map;
+import java.util.Set;
+
+public interface IFornecedorBusiness {
+    public void cadastrarFornecedor(String cnpj, String razaoSocial, Set<String> produtos);
+    public void salvar();
+    public Fornecedor buscarPorCnpj();
+    public Map<String, Fornecedor> listarTodos();
+    public void atualizar();
+    public void remover();
+    public void guardarDados();
 }
