bf33acf4976aa815cfaa779487ef9e1585c91795
MENOB <senjumenob@gmail.com>
2026-06-29 20:02:33 -0300

    Update(Main): Retirando business e repository de main

--- src/main/java/com/upe/loja/Main.java
+++ src/main/java/com/upe/loja/Main.java
@@ -1,14 +1,11 @@
 package com.upe.loja;
 
 import com.upe.loja.UI.MenuCategoria;
-import com.upe.loja.business.CategoriaBusiness;
-import com.upe.loja.repository.CategoriaRepository;
 
 public class Main{
   public static void main(String[] args) {
-    CategoriaRepository categoriaRepository= new CategoriaRepository();
-    CategoriaBusiness categoriaBusiness = new CategoriaBusiness(categoriaRepository);
-    Facade facade = new Facade(categoriaBusiness);
+
+    Facade facade = new Facade();
 
     MenuCategoria menuCategoria = new MenuCategoria(facade);
     menuCategoria.iniciar();
