ec59cf26e59ff21bf7071858e6a5799507d55a11
Raul Teotônio Soares Silva <raulteo987@gmail.com>
2026-06-29 19:34:07 -0300

    Add files via upload
    Adiciona Main e Facade do Cliente

---
+++ src/main/java/com/upe/loja/repository/Main.java
@@ -0,0 +1,12 @@
+package com.upe.loja;
+
+import com.upe.loja.UI.Menu;
+
+public class Main{
+  public static void main(String[] args) {
+    Facade facade = new Facade();
+
+    Menu menu = new Menu(facade);
+    menu.iniciar();
+  }
+}
