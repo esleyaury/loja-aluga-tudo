90d03df0d15269ffd82604fee85a7b0c65c52f7e
Gabriel <gabriellucius208@gmail.com>
2026-06-30 22:08:29 -0300

    refactor: remove atributo produto de Fornecedor e adequa camadas ao padrao da equipe

--- src/main/java/com/upe/loja/repository/entity/Fornecedor.java
+++ src/main/java/com/upe/loja/repository/entity/Fornecedor.java
@@ -11,27 +11,12 @@ public class Fornecedor {
         this.telefone = telefone;
     }
 
-    public String getCnpj() {
-         return cnpj; 
-        }
+    public String getCnpj() { return cnpj; }
+    public void setCnpj(String cnpj) { this.cnpj = cnpj; }
 
-    public void setCnpj(String cnpj) {
-         this.cnpj = cnpj; 
-        }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
 
-    public String getNome() {
-         return nome; 
-        }
-
-    public void setNome(String nome) { 
-        this.nome = nome; 
-        }
-
-    public String getTelefone() { 
-        return telefone; 
-        }
-        
-    public void setTelefone(String telefone) {
-         this.telefone = telefone; 
-        }
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
 }
